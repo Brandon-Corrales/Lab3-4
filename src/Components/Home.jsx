bb5961c1936d4f2f9b621cf8bdcbb8ad24645c8e
Brandon-Corrales <brandon.nunez.corrales@est.una.ac.cr>
2025-04-05 20:25:41 -0600

    Segundo commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,15 @@
+
+import { useContext } from 'react';
+import { AuthContext } from '../Context/AuthContext';
 import Welcome from './Welcome';
 
-export default function Home({ email }) {
+export default function Home() {
+  const { user } = useContext(AuthContext);
+
   return (
     <div>
-      <h1>Your at Home!</h1>
-      <Welcome userName={email} />
+      <h1>Your at Home!</h1> {}
+      <Welcome userName={user?.email} />
     </div>
   );
 }
-
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,53 +3,36 @@ import { AuthContext } from '../Context/AuthContext'
 import { useLogin } from '../hooks/useLogin'
 
 export default function Login() {
+  const { setUser } = useContext(AuthContext) 
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const [error, setError] = useState('')
+  
+  const { login } = useLogin()
 
-    // 3. Consumir el contexto
-    const { setUser } = useContext(AuthContext)
-    
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const [error, setError] = useState('')
-    
-    
-    const { login } = useLogin()
-    
-   
-
-    const handleLogin = () => {
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
-        const isSuccess = login(email, password);
-        if(!isSuccess)   
-        {
-            setError("Credenciales incorrectas");
-        }
-        else
-        {    
-            setUser(email)        
-        }
+  const handleLogin = () => {
+    const email = emailRef.current.value
+    const password = passwordRef.current.value
+    const isSuccess = login(email, password)
+    if (!isSuccess) {
+        setError("Credenciales incorrectas");
+      } else {
+        setUser({ email }); 
+        alert("Bienvenido " + email);
+      }
     }
 
-
-    return (
-        <>        
-              <h2>Login</h2>                      
-              <div>
-                <input 
-                type="email"
-                placeholder='Correo'
-                ref={emailRef}
-                />
-                <br/>
-                <input
-                type="password"
-                placeholder='Contraseña'
-                ref={passwordRef}
-                />
-                <br/>
-                <button onClick={handleLogin}>Ingresar</button>
-                { error && <p style={{ color: "red" }}>{error}</p> }
-              </div>                     
-        </>
-    )
+  return (
+    <>
+      <h2>Login</h2>
+      <div>
+        <input type="email" placeholder="Correo" ref={emailRef} />
+        <br />
+        <input type="password" placeholder="Contraseña" ref={passwordRef} />
+        <br />
+        <button onClick={handleLogin}>Ingresar</button>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </div>
+    </>
+  )
 }
